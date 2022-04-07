66288d571a0f211d3da8a2be37e086288f7112a1
marykarakida <102393986+marykarakida@users.noreply.github.com>
2022-04-07 13:44:11 -0300

    requisito: bônus

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ let numero = 0;
 let parCartas = [];
 let cartasViradas = 0;
 let jogadas = 0;
+let start = 0;
+let duration = 0;
+let intervalo = 0;
 
 pedirCartasNumero();
 
@@ -32,14 +35,20 @@ function randomizarCartas(numero) {
 }
 
 function inserirCartas(cartas) {
-    document.querySelector("ul").innerHTML = ""; //talvez mover depois para a funcao que for resetar o jogo
     for (var i = 0; i < cartas.length; i++) {
         document.querySelector("ul").innerHTML += `<li><div class="carta" onclick="clicarCarta(this)"><div class="face frente"><img src="asset/img/front.png" /></div><div class="face verso"><img src="asset/gif/${cartas[i]}.gif" /></div></div></li>`;
     }
+    start = Date.now();
+    intervalo = setInterval(medirTempo,1000,start);
 }
 
 function comparador() {
     return Math.random() - 0.5;
+}
+
+function medirTempo() {
+    duration = parseInt((Date.now() - start) / 1000);
+    document.querySelector(".tempo").innerHTML = `Timer: ${duration}s`;
 }
 
 function clicarCarta(carta) {
@@ -71,5 +80,29 @@ function desvirarCarta(parCartas) {
 }
 
 function finalizarJogo() {
-    alert(`Você ganhou em ${jogadas} jogadas!`);
+    alert(`Você ganhou em ${jogadas} jogadas e ${parseInt(duration)} segundos!`);
+    let novoJogo = prompt('Você quer jogar novamente? Se sim, digite "sim"; caso contrário, digite "não"' );
+    let respostaValida = false;
+    while (respostaValida === false) {
+        if (novoJogo === "sim") {
+            respostaValida = true;
+            resetarJogo();
+        } else if (novoJogo === "não"){
+            respostaValida = true;
+            alert("Obrigada por jogar!");
+            clearInterval(intervalo);
+        } else {
+            novoJogo = prompt('Respota inválida. Você quer jogar novamente? Se sim, digite "sim"; caso contrário, digite "não"' );
+        }
+
+    }
+}
+
+function resetarJogo() {
+    document.querySelector("ul").innerHTML = "";
+    numero = 0;
+    parCartas = [];
+    cartasViradas = 0;
+    jogadas = 0;
+    pedirCartasNumero();
 }

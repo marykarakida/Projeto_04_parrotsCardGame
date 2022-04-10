640a7e4b32bd8c254a34ffc297379af02890aad2
marykarakida <102393986+marykarakida@users.noreply.github.com>
2022-04-10 17:21:30 -0300

    detalhes adicionais v1
    + adicionado uma animacao para a distribuição de cartas no começo do jogo
    * melhorado o design do cronometro
    * o cronometro só começa a contagem depois que todas as cartas forem distribuídas

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ let numero = 0;
 let parCartas = [];
 let cartasViradas = 0;
 let jogadas = 0;
-let comeco, duracao, intervalo;
+let comeco, duracao, cronometro;
 let delay = false;
 
 pedirCartasNumero();
 
 function pedirCartasNumero() {
-    numero = Number(prompt("Olá, jogador(a)! Com quantas cartas você gostaria de jogar? Digite um número par, de 4 a 14"));
+    numero = Number(prompt("Olá, jogador(a)! Com quantas cartas você gostaria de jogar? \nDigite um número par, de 4 a 14."));
     let numeroValido = false;
     while (!numeroValido) {
         if (isNaN(numero) === false && numero <= 14 && numero >= 4 && numero % 2 === 0) {
@@ -33,19 +33,25 @@ function randomizarCartas(numero) {
 
 function inserirCartas(cartas) {
     for (var i = 0; i < cartas.length; i++) {
-        document.querySelector("ul").innerHTML += `<li><div class="carta" onclick="clicarCarta(this)"><div class="face frente"><img src="asset/img/front.png" /></div><div class="face verso"><img src="asset/gif/${cartas[i]}.gif" /></div></div></li>`;
+        setTimeout(function(carta) {
+            document.querySelector("ul").innerHTML += `<li><div class="carta" onclick="clicarCarta(this)"><div class="face frente"><img src="asset/img/front.png" /></div><div class="face verso"><img src="asset/gif/${carta}.gif" /></div></div></li>`;
+        }, i*200, cartas[i])
     }
-    comeco = Date.now();
-    intervalo = setInterval(medirTempo,1000,comeco);
+    let intervalo = () => {cronometro = setInterval(medirTempo,1000,Date.now())};
+    setTimeout(intervalo,i*200);
 }
 
 function comparador() {
     return Math.random() - 0.5;
 }
 
-function medirTempo() {
+function medirTempo(comeco) {
     duracao = parseInt((Date.now() - comeco) / 1000);
-    document.querySelector(".tempo").innerHTML = `Timer: ${duracao}s`;
+    document.querySelector(".tempo").innerHTML = `<ion-icon name="timer-outline"></ion-icon><span>  ${duracao}s  </span>`;
+}
+
+function mostrarRegras(icone) {
+    icone.classList.toggle("selecionado");
 }
 
 function clicarCarta(carta) {
@@ -79,8 +85,9 @@ function desvirarCarta(parCartas) {
 }
 
 function finalizarJogo() {
+    clearInterval(cronometro);
     alert(`Você ganhou em ${jogadas} jogadas e ${parseInt(duracao)} segundos!`);
-    let novoJogo = prompt('Você quer jogar novamente? Se sim, digite "sim"; caso contrário, digite "não"' );
+    let novoJogo = prompt('Você quer jogar novamente? \nSe sim, digite "sim"; caso contrário, digite "não"' );
     let respostaValida = false;
     while (respostaValida === false) {
         if (novoJogo === "sim") {
@@ -89,7 +96,6 @@ function finalizarJogo() {
         } else if (novoJogo === "não"){
             respostaValida = true;
             alert("Obrigada por jogar!");
-            clearInterval(intervalo);
         } else {
             novoJogo = prompt('Respota inválida. Você quer jogar novamente? Se sim, digite "sim"; caso contrário, digite "não"' );
         }
@@ -99,6 +105,7 @@ function finalizarJogo() {
 
 function resetarJogo() {
     document.querySelector("ul").innerHTML = "";
+    document.querySelector(".tempo").innerHTML = "";
     numero = 0;
     parCartas = [];
     cartasViradas = 0;

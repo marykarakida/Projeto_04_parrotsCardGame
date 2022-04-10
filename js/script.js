f70cd1438874b996646bc45bef69b45002592773
marykarakida <102393986+marykarakida@users.noreply.github.com>
2022-04-09 22:25:18 -0300

    correção e adição no requisito: clique na carta
    x corrigido erro em que cada clique contava com uma jogada, então clicar na mesma carta múltiplas vezes contava como várias jogadas
    x corrigido erro em que clicando na mesma carta duas ou mais vezes, o programa entendia que o jogador estava clicando duas cartas diferentes que formavam um par, e por isso não desvirava a carta
    + adicionado um delay que impede o jogador de clicar em uma carta antes que as cartas previamente selecionadas fossem desviradas

--- js/script.js
+++ js/script.js
@@ -2,16 +2,15 @@ let numero = 0;
 let parCartas = [];
 let cartasViradas = 0;
 let jogadas = 0;
-let start = 0;
-let duration = 0;
-let intervalo = 0;
+let comeco, duracao, intervalo;
+let delay = false;
 
 pedirCartasNumero();
 
 function pedirCartasNumero() {
     numero = Number(prompt("Olá, jogador(a)! Com quantas cartas você gostaria de jogar? Digite um número par, de 4 a 14"));
     let numeroValido = false;
-    while (numeroValido === false) {
+    while (!numeroValido) {
         if (isNaN(numero) === false && numero <= 14 && numero >= 4 && numero % 2 === 0) {
             numeroValido = true;
         } else {
@@ -25,12 +24,10 @@ function randomizarCartas(numero) {
     let cartas = ["bobrossparrot","explodyparrot","fiestaparrot","metalparrot","revertitparrot","tripletsparrot","unicornparrot"];
     cartas.sort(comparador);
     cartas = cartas.slice(0, numero/2);
- 
     for (var i = 0; i < numero/2; i++) {
         cartas.push(cartas[i]);
     }
     cartas.sort(comparador);
-    
     inserirCartas(cartas);
 }
 
@@ -38,8 +35,8 @@ function inserirCartas(cartas) {
     for (var i = 0; i < cartas.length; i++) {
         document.querySelector("ul").innerHTML += `<li><div class="carta" onclick="clicarCarta(this)"><div class="face frente"><img src="asset/img/front.png" /></div><div class="face verso"><img src="asset/gif/${cartas[i]}.gif" /></div></div></li>`;
     }
-    start = Date.now();
-    intervalo = setInterval(medirTempo,1000,start);
+    comeco = Date.now();
+    intervalo = setInterval(medirTempo,1000,comeco);
 }
 
 function comparador() {
@@ -47,14 +44,16 @@ function comparador() {
 }
 
 function medirTempo() {
-    duration = parseInt((Date.now() - start) / 1000);
-    document.querySelector(".tempo").innerHTML = `Timer: ${duration}s`;
+    duracao = parseInt((Date.now() - comeco) / 1000);
+    document.querySelector(".tempo").innerHTML = `Timer: ${duracao}s`;
 }
 
 function clicarCarta(carta) {
-    jogadas++;
-    carta.classList.add("virada");
-    parCartas.push(carta);
+    if (!carta.classList.contains("virada") && !delay) {
+        carta.classList.add("virada");
+        parCartas.push(carta);
+        jogadas++;
+    }
     if (parCartas.length === 2) {
         conferirPar(parCartas);
         parCartas = [];
@@ -63,11 +62,10 @@ function clicarCarta(carta) {
 
 function conferirPar(parCartas) {
     if (parCartas[0].innerHTML !== parCartas[1].innerHTML) {
+        delay = true;
         setTimeout(desvirarCarta, 1000, parCartas);
-        return;
-    } else {
-        cartasViradas += 2;
-    }
+    } 
+    cartasViradas = document.querySelectorAll(".virada").length;
     if (cartasViradas === numero) {
         finalizarJogo();
     }
@@ -77,10 +75,11 @@ function desvirarCarta(parCartas) {
     for (var i = 0; i < parCartas.length; i++) {
         parCartas[i].classList.remove("virada");
     }
+    delay = false;
 }
 
 function finalizarJogo() {
-    alert(`Você ganhou em ${jogadas} jogadas e ${parseInt(duration)} segundos!`);
+    alert(`Você ganhou em ${jogadas} jogadas e ${parseInt(duracao)} segundos!`);
     let novoJogo = prompt('Você quer jogar novamente? Se sim, digite "sim"; caso contrário, digite "não"' );
     let respostaValida = false;
     while (respostaValida === false) {
